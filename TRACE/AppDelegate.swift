09ffff4c7eb5a4cb91c422ec97a3faf2c22964d7
jiyong <wldyd2113@gmail.com>
2025-10-03 16:53:25 +0900

    feat: fibase analytics, Crashlyics추가

--- TRACE/AppDelegate.swift
+++ TRACE/AppDelegate.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-
+import FirebaseCore
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Realm 초기화
         _ = RealmManager.shared
-
+        FirebaseApp.configure()
         return true
     }
 
