37b40e46e5438e2953aa4f9c15b6b2ac2d4df871
jiyong <wldyd2113@gmail.com>
2025-10-21 18:26:15 +0900

    package: FSCalendar

---
+++ TRACE/View/TravelCalendar/TravelCalendarViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TravelCalendarViewController.swift
+//  TRACE
+//
+//  Created by 차지용 on 10/21/25.
+//
+
+import UIKit
+
+class TravelCalendarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
