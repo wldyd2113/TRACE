9fef58aaf5de4e8a112274eba1aabeceeab7ea29
jiyong <wldyd2113@gmail.com>
2025-10-30 11:01:32 +0900

    feat: Widget

---
+++ TraceBasicIntentWidget/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  TraceBasicIntentWidget
+//
+//  Created by 차지용 on 10/30/25.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource { "Configuration" }
+    static var description: IntentDescription { "This is an example widget." }
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
---
+++ TraceBasicIntentWidget/TraceBasicIntentWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  TraceBasicIntentWidgetBundle.swift
+//  TraceBasicIntentWidget
+//
+//  Created by 차지용 on 10/30/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct TraceBasicIntentWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        TraceBasicIntentWidget()
+    }
+}
